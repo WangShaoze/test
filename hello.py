4de0b3a050a6f4e74cf2d26d194e5c2d59f65613
WangShaoze <2847832624@qq.com>
2023-07-19 08:51:58 +0800

    基础框架搭建

--- hello.py
+++
@@ -1,5 +0,0 @@
-print('hello you are right!')
-
-for i in range(89):
-    print('you are so sweet!!')
-
---
+++ util/other_frameUtil/loginFrame.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+class LoginFrame(Frame):
+    def __init__(self, master, **kwargs):
+        super(LoginFrame, self).__init__(master, **kwargs)
+
+        # 初始化一些基本变量
+
+        self.widget()
+
+    def widget(self):
+        pass
