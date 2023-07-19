271a0b16e9b936d233627b16ca6415df8d6e6585
WangShaoze <2847832624@qq.com>
2023-07-19 22:19:50 +0800

    与数据库有关的文件

---
+++ util/mysqlDB_util/sqlite_db_util.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 建立连接
+engine = create_engine('sqlite:///mydatabase.db', echo=True)
+
+# 创建Session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# 创建Base类
+Base = declarative_base()
+
+# 定义模型类
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+# 创建表
+Base.metadata.create_all(engine)
+
+# 查询数据
+users = session.query(User).all()
+for user in users:
+    print(f"User ID: {user.id}, Name: {user.name}")
+
+# 添加数据
+new_user = User(name="John Doe")
+session.add(new_user)
+session.commit()
+
+# 关闭Session
+session.close()
